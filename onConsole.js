90deb86cd737c4e9428943e63b4711a3505bf378
carl0sfelipe <37373901+carl0sfelipe@users.noreply.github.com>
2021-09-16 19:29:50 -0300

    Create onConsole.js

---
+++ onConsole.js
@@ -0,0 +1,3 @@
+node = document.getElementsByClassName('a4gq8e-aVTXAb-haAclf-jRmmHf-hSRGPd');
+let arr = Array.prototype.slice.call(node);
+let cordenates = arr.map((el)=>el.href).map((el)=>el.split("?")[0]).map((el)=>el.split("d-")).map((el)=> [el[1],el[2]]).map((el)=>[ el[0].substring(0, el[0].length - 2), el[1]])
